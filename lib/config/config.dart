f5797469ec1f8636816b8af21bb6ceb4cd8e855c
gohoy <2738430398@qq.com>
2024-06-26 22:04:18 +0800

    打包配置

--- lib/config/config.dart
+++ lib/config/config.dart
@@ -1,3 +1,4 @@
 class Config {
-  static String baseUrl = "http://127.0.0.1:4523/m1/4710834-0-default";
+  static String baseUrl = "https://mock.apipark.cn/m1/4710834-0-default";
+  // static String baseUrl = "http://127.0.0.1:4523/m1/4710834-0-default";
 }
